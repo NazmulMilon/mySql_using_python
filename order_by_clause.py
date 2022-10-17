96ac012cada67ce482584a98cf4ddbc02f875876
nazmulmilon <milonsarker73@gmail.com>
2022-10-17 11:30:29 +0600

    fetch data in mysql using python

---
+++ order_by_clause.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="15101997",
+    database="db_school"
+)
+
+cursor_object = dataBase.cursor()
+
+query = "SELECT * FROM students ORDER BY name"
+cursor_object.execute(query)
+
+my_result = cursor_object.fetchall()
+
+for data in my_result:
+    print(data)
+
+dataBase.close()
+
