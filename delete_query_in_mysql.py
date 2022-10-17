c52fd0f926317aa1b8a8f8684e9cf5c688e87be1
nazmulmilon <milonsarker73@gmail.com>
2022-10-17 11:57:03 +0600

    delete query in mysql using python

---
+++ delete_query_in_mysql.py
@@ -0,0 +1,23 @@
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="15101997",
+    database="db_school"
+)
+
+cursor_object = dataBase.cursor()
+
+query = "DELETE FROM students WHERE age='26'"
+cursor_object.execute(query)
+
+dataBase.commit()
+
+my_result = cursor_object.fetchall()
+for data in my_result:
+    print(data)
+
+# dataBase.close()
+
